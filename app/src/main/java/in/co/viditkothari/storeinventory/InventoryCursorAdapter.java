9f62af64c3afb57e440a5a1adf31107b14facb03
Vidit Kothari <kothari.vidit@gmail.com>
2017-01-29 13:08:22 +0530

    v6: Ready for submission

--- app/src/main/java/in/co/viditkothari/storeinventory/InventoryCursorAdapter.java
+++ app/src/main/java/in/co/viditkothari/storeinventory/InventoryCursorAdapter.java
@@ -1,6 +1,8 @@
 package in.co.viditkothari.storeinventory;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
@@ -36,7 +38,7 @@ public class InventoryCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
         ImageView imgv_product_image = (ImageView) view.findViewById(R.id.product_image);
         TextView tv_product_name = (TextView) view.findViewById(R.id.product_name);
         TextView tv_product_quantity = (TextView) view.findViewById(R.id.product_quantity);
@@ -44,25 +46,26 @@ public class InventoryCursorAdapter extends CursorAdapter {
         TextView tv_product_description = (TextView) view.findViewById(R.id.product_description);
         TextView tv_product_sell = (TextView) view.findViewById(R.id.btn_product_sell);
 
-
         int product_name_ColIndex = cursor.getColumnIndex(InventoryTable.COL_PRODUCT_NAME);
         int product_image_ColIndex = cursor.getColumnIndex(InventoryTable.COL_PRODUCT_IMAGE_URI);
         int product_desc_ColIndex = cursor.getColumnIndex(InventoryTable.COL_PRODUCT_DESC);
         int product_quantity_ColIndex = cursor.getColumnIndex(InventoryTable.COL_PRODUCT_QUANTITY);
         int product_price_ColIndex = cursor.getColumnIndex(InventoryTable.COL_PRODUCT_PRICE);
 
-        Log.v("VVVV Cursor Logging:",cursor.toString());
+        final int _id = cursor.getInt(cursor.getColumnIndex(InventoryTable._ID));
+        tv_product_quantity.setTag(_id);
+
         String product_name = cursor.getString(product_name_ColIndex);
         String product_image = cursor.getString(product_image_ColIndex);
         String product_desc = cursor.getString(product_desc_ColIndex);
-        int product_quantity = cursor.getInt(product_quantity_ColIndex);
+        final int product_quantity = cursor.getInt(product_quantity_ColIndex);
         Double product_price = cursor.getDouble(product_price_ColIndex);
-        Log.i("product_name: ",cursor.getColumnName(0) + " : " + cursor.getColumnCount());
-        Log.i("product_image: ",cursor.getCount() + " : " + product_image);
-        Log.i("product_description: ",product_desc);
-        Log.i("product_quantity: ",String.valueOf(product_quantity));
-        Log.i("product_price: ",String.valueOf(product_price));
-
+        Log.v("VVVV Cursor Logging:",cursor.toString());
+        Log.v("product_name: ",cursor.getColumnName(0) + " : " + cursor.getColumnCount());
+        Log.v("product_image: ",cursor.getCount() + " : " + product_image);
+        Log.v("product_description: ",product_desc);
+        Log.v("product_quantity: ",String.valueOf(product_quantity));
+        Log.v("product_price: ",String.valueOf(product_price));
 
         Bitmap bitmap;
         try {
@@ -76,5 +79,21 @@ public class InventoryCursorAdapter extends CursorAdapter {
         tv_product_description.setText(product_desc);
         tv_product_quantity.setText(String.valueOf(product_quantity));
         tv_product_price.setText(String.valueOf(product_price));
+
+        // Implementing functionality for "sell button" on a list Item.
+        tv_product_sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(product_quantity>0){
+                    Uri mCurrentCaseUri = ContentUris.withAppendedId(InventoryTable.CONTENT_URI, _id);
+                    ContentValues values = new ContentValues();
+                    values.put(InventoryTable.COL_PRODUCT_QUANTITY, (product_quantity-1));
+                    context.getContentResolver().update(mCurrentCaseUri, values, null, null);
+                }
+                else
+                    Toast.makeText(context,"out_of_stock", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/in/co/viditkothari/storeinventory/MainActivity.java
+++ app/src/main/java/in/co/viditkothari/storeinventory/MainActivity.java
@@ -50,30 +50,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 Intent intent = new Intent(MainActivity.this, DetailActivity.class);
                 final Uri currentUri = ContentUris.withAppendedId(InventoryTable.CONTENT_URI, id);
 
-                // Implementing functionality for "sell button" on a list Item.
-                listView.findViewById(R.id.btn_product_sell).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String[] projection = {
-                                InventoryTable._ID,
-                                InventoryTable.COL_PRODUCT_QUANTITY};
-                        Cursor c=getContentResolver().query(currentUri,projection,null,null,null);
-                        int product_quantity_ColIndex = c.getColumnIndex(InventoryTable.COL_PRODUCT_QUANTITY);
-                        int product_quantity = c.getInt(product_quantity_ColIndex);
-                        ContentValues values = new ContentValues();
-                        if(product_quantity>0) {
-                            values.put(InventoryTable.COL_PRODUCT_QUANTITY, product_quantity - 1);
-                            int a = getContentResolver().update(currentUri,values,null,null);
-                        }
-                        // ((TextView)listView.findViewById(R.id.product_quantity)).setText(String.valueOf(product_quantity));
-                        //
-                       /* int originalValue = Integer.parseInt(tv_product_quantity.getText().toString().trim());
-                                values.put(InventoryTable.COL_PRODUCT_QUANTITY, newValue);
-
-                            rowsAffected = getContentResolver().update(currentUri, values, null, null);*/
-                    }
-                });
-
                 // Set the URI on the data field of the intent
                 intent.setData(currentUri);
                 startActivity(intent);
